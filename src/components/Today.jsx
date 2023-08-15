cfefb8e25ef732728e41432134b5bcb68217eed0
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-08-16 00:49:07 +0530

    fix

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -5,7 +5,7 @@ import Newcontent from './Newcontent'
 const Today = () => {
   return (
     <div id='home' className='max-w-[1640px]  mx-auto p-4 py-12'>
-        <Headline title='TRENDING'/>
+        <Headline title='TRENDING' />
         <Newcontent/>
     </div>
   )
