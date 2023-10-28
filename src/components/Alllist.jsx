9b6679822c66f0596bc5c89e86bc284a43befe4f
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-10-28 21:37:43 +0530

    add pagination

--- src/components/Alllist.jsx
+++ src/components/Alllist.jsx
@@ -4,14 +4,19 @@ import React from "react";
 import "./Alllist.css";
 import "./Allcard.css";
 import { useEffect, useState } from "react";
-
+import { Pagination } from "@mui/material";
 // import { getFirestore, onSnapshot, collection, query, orderBy, limit, getDocs   } from 'firebase/firestore';
 
 import { getDocs, collection, query, orderBy } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 
+
 const Alllist = () => {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  const [pageCount, setPageCount] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [allPost, setAllPost] = useState([]);
+  const [data, setData] = useState([[{ name: "amila" }]]);
 
   useEffect(() => {
     const q = query(collection(db, "allphoto"), orderBy("description", "desc")); // Adjust the limit as needed
@@ -26,23 +31,53 @@ const Alllist = () => {
         });
         setData(fetchData);
         console.log(fetchData);
+
+        // add pagination --------------------
+        const data = fetchData;
+
+        // Divide the data into chunks of 10
+        const temp = [];
+        for (let i = 0; i < Math.ceil(data.length / 25); i++) {
+          temp[i] = data.slice(i * 25, (i + 1) * 25);
+        }
+
+        setAllPost(temp);
+        setPageCount(temp.length);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  const handlePaginationChange = (e, page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="list-coc">
-      {data.map(({ imageUrl, baseurl }, index) => (
-        <div className="card" key={index}>
-          <div className="card_image">
-            <a href={baseurl}>
-              <img src={imageUrl} />
-            </a>
-          </div>
-        </div>
-      ))}
+    <div>
+      <div className="list-coc">
+        {allPost[currentPage - 1] &&
+          allPost[currentPage - 1].map((item, index) => (
+            <div className="card" key={index}>
+              <div className="card_image">
+                <a href={item.baseurl}>
+                  <img src={item.imageUrl} />
+                </a>
+                
+              </div>
+            </div>
+          ))}
+      </div>
+      <Pagination
+        count={pageCount}
+        variant="outlined"
+        onChange={handlePaginationChange}
+        shape="rounded"
+        color="primary"
+      
+        className="bg-white px-2 py-2 mx-3 "
+      />
     </div>
   );
 };
 
 export default Alllist;
+
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDdyTqvViyqtLETSrNMzqFP-xM0NIa9lQ8",
-    authDomain: "clashlayouts-9d19d.firebaseapp.com",
-    projectId: "clashlayouts-9d19d",
-    storageBucket: "clashlayouts-9d19d.appspot.com",
-    messagingSenderId: "1073716509146",
-    appId: "1:1073716509146:web:56713b2e4bf310187b28e3"
-  };
+  apiKey: "AIzaSyDdyTqvViyqtLETSrNMzqFP-xM0NIa9lQ8",
+  authDomain: "clashlayouts-9d19d.firebaseapp.com",
+  projectId: "clashlayouts-9d19d",
+  storageBucket: "clashlayouts-9d19d.appspot.com",
+  messagingSenderId: "1073716509146",
+  appId: "1:1073716509146:web:56713b2e4bf310187b28e3"
+};
 
 // Initialize Firebase
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
