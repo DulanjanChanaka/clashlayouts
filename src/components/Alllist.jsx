97802db778e0b0831d956dc06f89a42d0f346f0d
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-10-28 20:48:20 +0530

    change fetching function

--- src/components/Alllist.jsx
+++ src/components/Alllist.jsx
@@ -1,53 +1,48 @@
-"use client"
+"use client";
 import React from "react";
 
 import "./Alllist.css";
 import "./Allcard.css";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 // import { getFirestore, onSnapshot, collection, query, orderBy, limit, getDocs   } from 'firebase/firestore';
 
-import {  onSnapshot, collection, query, orderBy } from 'firebase/firestore';
+import { getDocs, collection, query, orderBy } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 
-
-
 const Alllist = () => {
-
-  const [data, setData] = useState ([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    const q = query(collection(db, 'allphoto'), orderBy("description", "desc")); // Adjust the limit as needed
+    const q = query(collection(db, "allphoto"), orderBy("description", "desc")); // Adjust the limit as needed
 
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      const fetchedData = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setData(fetchedData);
-      console.log(data)
-    });
-
-    return () => {
-      unsubscribe();
-      
-    };
+    getDocs(q)
+      .then((querySnapshot) => {
+        const fetchData = [];
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          fetchData.push(doc.data());
+          // console.log(doc.id, " => ", doc.data());
+        });
+        setData(fetchData);
+        console.log(fetchData);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
-
-    
-      return (
-        <div className="list-coc">
-          {data.map(({ imageUrl, baseurl }, index) => (
-            <div className="card" key={index}>
-              <div className="card_image">
-                <a href={baseurl}><img src={imageUrl} /></a>
-              </div>
-            </div>
-          ))}
+  return (
+    <div className="list-coc">
+      {data.map(({ imageUrl, baseurl }, index) => (
+        <div className="card" key={index}>
+          <div className="card_image">
+            <a href={baseurl}>
+              <img src={imageUrl} />
+            </a>
+          </div>
         </div>
-      );
+      ))}
+    </div>
+  );
 };
 
 export default Alllist;
-
--- src/components/Newcontent.jsx
+++ src/components/Newcontent.jsx
@@ -1,37 +1,35 @@
-"use client"
+"use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { useEffect, useState } from 'react';
-import { Navigation , Autoplay  } from "swiper/modules";
-import {  onSnapshot, collection,  } from 'firebase/firestore';
+import { useEffect, useState } from "react";
+import { Navigation, Autoplay } from "swiper/modules";
+import { query, getDocs, collection } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 
-
-const Newcontent = ({ title= [] }) => {
-
-  const [data, setData] = useState ([])
+const Newcontent = ({ title = [] }) => {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, 'topphoto'), (snapshot) => {
-      const data = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setData(data);
-    });
+    const q = query(collection(db, "topphoto"));
 
-    return () => {
-      unsubscribe();
-    };
+    getDocs(q)
+      .then((querySnapshot) => {
+        const fetchData = [];
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          fetchData.push(doc.data());
+          // console.log(doc.id, " => ", doc.data());
+        });
+        setData(fetchData);
+        console.log(fetchData);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
-
-      
-
-      const [slidesPerView, setSlidesPerView] = useState(4);
+  const [slidesPerView, setSlidesPerView] = useState(4);
 
   useEffect(() => {
     // Update the slidesPerView based on window width on the client-side
@@ -39,29 +37,37 @@ const Newcontent = ({ title= [] }) => {
       setSlidesPerView(window.innerWidth < 600 ? 1 : 3);
     }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Call the handleResize function initially to set the initial slidesPerView
     handleResize();
 
     // Clean up the event listener when the component unmounts
     return () => {
-      window.removeEventListener('resize', handleResize);}
-
-
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
     <div className="w-full h-50vh m-0 p-0">
       <h2>{title}</h2>
       {/* Render the Swiper directly without checking typeof window */}
-      <Swiper className="w-full p-0" slidesPerView={slidesPerView} spaceBetween={10} navigation autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }} modules={[Navigation, Autoplay]}>
+      <Swiper
+        className="w-full p-0"
+        slidesPerView={slidesPerView}
+        spaceBetween={10}
+        navigation
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Navigation, Autoplay]}
+      >
         {data?.map(({ imageUrl, baseurl }, index) => (
           <SwiperSlide className="w-auto h-auto" key={index}>
-            <a href={baseurl}><img src={imageUrl}/></a>
+            <a href={baseurl}>
+              <img src={imageUrl} />
+            </a>
           </SwiperSlide>
         ))}
       </Swiper>
@@ -70,4 +76,3 @@ const Newcontent = ({ title= [] }) => {
 };
 
 export default Newcontent;
-
