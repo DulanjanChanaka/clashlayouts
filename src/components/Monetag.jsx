303dcc8c4c9827f637405d19a7ef3e1aa9f8eca0
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-04 00:04:59 +0530

    monetag

--- src/components/Monetag.jsx
+++ src/components/Monetag.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Monetag() {
   return (
-    <div className='text-center text-5xl' ><h1>
-        "Hello, Monetag!"
-        </h1></div>
+    <div className='text-center text-5xl' >
+        <h1>&nbsp;&ldquo;Hello, Monetag!&rdquo;</h1>
+    </div>
   )
 }
 
